542fc5ed2e0a73cd35a565d92b4f7e9e9899002e
Ross Cousens <ross@hatesmail.com>
2015-03-17 00:34:20 +1000

    Null instead of 0

--- src/single-list/single-linked-list.c
+++ src/single-list/single-linked-list.c
@@ -11,14 +11,14 @@ sll_node* sll_new_list(int value)
     sll_node* root;
     root = malloc(sizeof(sll_node));
     root->value = value;
-    root->next = 0;
+    root->next = NULL;
     return root;
 }
 
 sll_node* sll_traverse_to_end(sll_node* list)
 {
-    if (list != 0) {
-        while(list->next != 0) {
+    if (list) {
+        while(list->next != NULL) {
             list = list->next;
         }
     }
@@ -35,12 +35,12 @@ void sll_add_to_list(sll_node* list, int value)
     new_node = malloc(sizeof(sll_node));
     final_node->next = new_node;
     new_node->value = value;
-    new_node->next = 0;
+    new_node->next = NULL;
 }
 
 void sll_print_list(sll_node* list)
 {
-    for(; list != 0; list = list->next) {
+    for(; list; list = list->next) {
         printf("Location: %p\t", list);
         printf("Value: %d\n", list->value);
     }
@@ -50,7 +50,7 @@ void sll_destroy_list(sll_node* list)
 {
     sll_node* next;
 
-    for(; list != 0; list = next) {
+    for(; list; list = next) {
         next = list->next;
         free(list);
     }
